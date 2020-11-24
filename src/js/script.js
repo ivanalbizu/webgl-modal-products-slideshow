90f7b9a565a26b4b67c6e16fc9e066fc4ebc4a28
ivanalbizu <ivan.albizu@gmail.com>
2020-11-24 22:38:25 +0100

    Modals

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-import { Curtains, Plane } from 'curtainsjs';
+import 'regenerator-runtime/runtime'
+import { Curtains, Plane } from 'curtainsjs'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 
@@ -9,17 +10,18 @@ class WebglSlides {
 		this.planeElement = set.planeElement
 		this.multiTexturesPlane = null
 		this.touchStartY = 0
+		this.countSlides = set.planeElement.querySelectorAll("img").length - 1
 		this.slidesState = {
-			activeTextureIndex: 1,
+			activeTextureIndex: Math.ceil((set.progress.value / 100) * this.countSlides),
 			nextTextureIndex: null,
-			maxTextures: set.planeElement.querySelectorAll("img").length - 1, // -1 to displacement
+			maxTextures: this.countSlides,
 			navs: set.navs,
 
 			isChanging: false,
 			transitionTimer: 0,
 
 			progress: set.progress,
-			value: 16,
+			value: set.progress.value,
 			next: null
 		}
 		this.params = {
@@ -49,8 +51,8 @@ class WebglSlides {
 			watchScroll: false,
 			pixelRatio: Math.min(1.5, window.devicePixelRatio)
 		})
-		this.curtains.onError(() => this.error());
-		this.curtains.onContextLost(() => this.restoreContext());
+		this.curtains.onError(() => this.error())
+		this.curtains.onContextLost(() => this.restoreContext())
 	}
 
 	initPlane () {
@@ -81,22 +83,22 @@ class WebglSlides {
 	update () {
 		this.multiTexturesPlane.onRender(() => {
 			if (this.slidesState.isChanging) {
-				this.slidesState.transitionTimer += (90 - this.slidesState.transitionTimer) * 0.1;
+				this.slidesState.transitionTimer += (90 - this.slidesState.transitionTimer) * 0.1
 
 				if (this.slidesState.transitionTimer >= 88.5 && this.slidesState.transitionTimer !== 90) {
-					this.slidesState.transitionTimer = 90;
+					this.slidesState.transitionTimer = 90
 				}
 			}
 
-			this.multiTexturesPlane.uniforms.transitionTimer.value = this.slidesState.transitionTimer;
-		});
+			this.multiTexturesPlane.uniforms.transitionTimer.value = this.slidesState.transitionTimer
+		})
 	}
 
 	arrowEvent (activeTexture, nextTexture) {
 		this.slidesState.navs.forEach(nav => {
 			nav.addEventListener('click', event => {
 
-				const to = event.target.getAttribute('data-goto');
+				const to = event.target.getAttribute('data-goto')
 				this._animate(to, activeTexture, nextTexture)
 
 			})
@@ -106,23 +108,24 @@ class WebglSlides {
 	keyboardEvent (activeTexture, nextTexture) {
 		const that = this
 		document.addEventListener('keyup', event => {
-			let to = undefined
-			switch (event.key) {
-				case 'ArrowDown':
-					to = 'next'
-					break
-				case 'ArrowUp':
-					to = 'prev'
-					break
+			if (document.querySelector('.modal--active')) {
+				let to = undefined
+				switch (event.key) {
+					case 'ArrowDown':
+						to = 'next'
+						break
+					case 'ArrowUp':
+						to = 'prev'
+						break
+				}
+				that._animate(to, activeTexture, nextTexture)
 			}
-
-			that._animate(to, activeTexture, nextTexture)
 		})
 	}
 
 	wheelEvent (activeTexture, nextTexture) {
 		const that = this
-		this.planeElement.addEventListener('wheel', event => {
+		this.canvas.closest('.modal').addEventListener('wheel', event => {
 			let to = undefined
 			if (event.deltaY > 0) {
 				to = 'next'
@@ -152,8 +155,8 @@ class WebglSlides {
 			}
 		}
 		this.slidesState.progress.value = `${this.slidesState.value}`
-		console.log('this.slidesState.value :>> ', this.slidesState.value);
-		if (this.slidesState.value !== this.slidesState.steps) requestAnimationFrame(() => this._animateProgressBar());
+
+		if (this.slidesState.value !== this.slidesState.steps) requestAnimationFrame(() => this._animateProgressBar())
 	}
 
 	_animate (to, activeTexture, nextTexture) {
@@ -161,26 +164,26 @@ class WebglSlides {
 
 			this.curtains.enableDrawing()
 
-			this.slidesState.isChanging = true;
+			this.slidesState.isChanging = true
 
-			this._navigationDirection(to);
+			this._navigationDirection(to)
 			this.slidesState.steps = Math.ceil((100 / this.slidesState.maxTextures) * this.slidesState.nextTextureIndex)
 			requestAnimationFrame(() => this._animateProgressBar())
 
-			nextTexture.setSource(this.multiTexturesPlane.images[this.slidesState.nextTextureIndex]);
+			nextTexture.setSource(this.multiTexturesPlane.images[this.slidesState.nextTextureIndex])
 
 			setTimeout(() => {
 
-				this.curtains.disableDrawing();
+				this.curtains.disableDrawing()
 
-				this.slidesState.isChanging = false;
-				this.slidesState.activeTextureIndex = this.slidesState.nextTextureIndex;
+				this.slidesState.isChanging = false
+				this.slidesState.activeTextureIndex = this.slidesState.nextTextureIndex
 
-				activeTexture.setSource(this.multiTexturesPlane.images[this.slidesState.activeTextureIndex]);
+				activeTexture.setSource(this.multiTexturesPlane.images[this.slidesState.activeTextureIndex])
 
-				this.slidesState.transitionTimer = 0;
+				this.slidesState.transitionTimer = 0
 
-			}, 600);
+			}, 600)
 		}
 	}
 
@@ -213,71 +216,276 @@ class WebglSlides {
   }
 
 	error () {
-		document.body.classList.add("no-curtains", "image-1");
+		document.body.classList.add("no-curtains", "image-1")
 
 		this.slidesState.navs.forEach(nav => {
 			nav.addEventListener("click", event => {
-				const to = event.target.getAttribute('data-goto');
-				this._navigationDirection(to);
+				const to = event.target.getAttribute('data-goto')
+				this._navigationDirection(to)
 
-				document.body.classList.remove("image-1", "image-2", "image-3", "image-4");
-				document.body.classList.add("image-" + this.slidesState.nextTextureIndex);
+				document.body.classList.remove("image-1", "image-2", "image-3", "image-4")
+				document.body.classList.add("image-" + this.slidesState.nextTextureIndex)
 
-				this.slidesState.activeTextureIndex = this.slidesState.nextTextureIndex;
+				this.slidesState.activeTextureIndex = this.slidesState.nextTextureIndex
 
-			});
+			})
 		})
 	}
 
 	restoreContext () {
-		this.curtains.restoreContext();
+		this.curtains.restoreContext()
 	}
 
-	// Not necesary, only for change Displacements Texture
 	removePlanes () {
-		this.slidesState.value = 16
-		this.curtains.dispose();
+		this.curtains.dispose()
 	}
+}
+
+const isMobileDevice = () => {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const modals = document.querySelectorAll('[data-modal]')
+  modals.forEach(modal => modal.addEventListener('click', openModal, false))
+
+  const closes = document.querySelectorAll('.modal-close')
+  closes.forEach(close => close.addEventListener('click', closeModal, false))
+
+  if (!isMobileDevice()) {
+    const wakeyFixRequired = 'CSS' in window && typeof CSS.supports === 'function' && !CSS.supports('-webkit-appearance', 'none');
+
+    const ROOT_CLASS = 'scrollscreen';
+    document.documentElement.classList.add('custom-scroll');
+
+    const createElement = (tag, name) => {
+      const el = document.createElement(tag);
+      el.className = `${ROOT_CLASS}--${name}`;
+      return el;
+    }
+
+    const createScrollscreen = (root) => {
+
+      const fragment = document.createDocumentFragment();
+      [...root.childNodes].forEach(child => {
+        fragment.appendChild(child);
+      });
+
+      const content = createElement('div', 'content');
+      content.appendChild(fragment);
+      root.appendChild(content);
+
+      const track = createElement('div', 'track');
+      root.appendChild(track);
+
+      const slider = createElement('div', 'slider');
+      track.appendChild(slider);
+
+      let pendingFrame = null;
+
+      const redraw = () => {
+
+        cancelAnimationFrame(pendingFrame);
+
+        pendingFrame = requestAnimationFrame(() => {
+
+          const contentHeight = content.scrollHeight;
+          const containerHeight = root.offsetHeight;
+          const percentageVisible = containerHeight / contentHeight;
+          const sliderHeight = percentageVisible * containerHeight;
+          const percentageOffset = content.scrollTop / (contentHeight - containerHeight);
+          const sliderOffset = percentageOffset * (containerHeight - sliderHeight);
+
+          track.style.opacity = percentageVisible === 1 ? 0 : 1;
+
+          slider.style.cssText = `
+            height: ${sliderHeight}px;
+            transform: translateY(${sliderOffset}px);
+          `;
+        });
+
+      }
+
+      content.addEventListener('scroll', redraw);
+      window.addEventListener('resize', redraw);
+
+      redraw();
+
+      if (!wakeyFixRequired) return;
+
+      const wakey = () => {
+        requestAnimationFrame(() => {
+          const offset = content.scrollTop;
+          content.scrollTop = offset + 1;
+          content.scrollTop = offset;
+        });
+      }
+
+      root.addEventListener('mouseenter', wakey);
+
+      wakey();
+    }
+
+    [...document.querySelectorAll(`.${ROOT_CLASS}`)].forEach(createScrollscreen);
+
+  }
+
+})
+
+
+const requestAnimationFramePromise = () => {
+  return new Promise(resolve => requestAnimationFrame(resolve))
+}
+const transitionEndPromise = (element, last) => {
+  return new Promise(resolve => {
+    element.addEventListener('transitionend', event => {
+      if (event.propertyName !== last) return
+      element.removeEventListener('transitionend', this)
+      resolve()
+    }, true)
+  })
+}
+const animate = async (element, stylz, last) => {
+  Object.assign(element.style, stylz)
+  return transitionEndPromise(element, last).then(_ => requestAnimationFramePromise())
+}
+
+const transtionFrom = (el, start, appendTo) => {
+	const styles = window.getComputedStyle(el)
+  const rect = start.getBoundingClientRect()
+  const style = {
+    left: `${rect.left}px`,
+    top: `${rect.top}px`,
+    width: `${rect.width}px`,
+		height: `${rect.height}px`,
+		fontSize: styles.getPropertyValue('font-size'),
+		position: `fixed`,
+		backgroundSize: `cover`
+  }
+  Object.assign(el.style, style)
+  appendTo.parentNode.append(el)
+}
+
+const transitionTo = (el, cssTransition) => {
+  const styles = window.getComputedStyle(el)
+  const rect = el.getBoundingClientRect()
+  return { 
+    transition: `${cssTransition}`,
+    top: `${rect.top}px`,
+    left: `${rect.left}px`,
+    width: `${rect.width}px`,
+    height: `${rect.height}px`,
+    fontSize: styles.getPropertyValue('font-size')
+  }
+}
+
+let modalState = {}
+let productState = {}
+let webgl = {}
+
+
+const openModal = event => {
+	document.body.classList.add('is-fade-in')
+
+	const product = event.target.closest('.product')
+	const dataModalID = 'modal'+product.getAttribute('data-modal')
+	const modal = document.querySelector('#'+dataModalID)
+  product.style.pointerEvents = 'none'
+	product.setAttribute('data-modal-active', true)
+
+	productState = {
+		product,
+		img: product.querySelector('.product__img'),
+		title: product.querySelector('.product__title'),
+		price: product.querySelector('.product__price')
+	}
+
+	modalState = {
+		modal,
+		img: modal.querySelector('.modal-image'),
+		title: modal.querySelector('.product__title'),
+		price: modal.querySelector('.product__price')
+	}
+
+	const cloneProductImg = productState.img.cloneNode(true)
+	cloneProductImg.style.borderColor = 'transparent'
+  transtionFrom(cloneProductImg, productState.img, product)
+	const cloneProductTitle = productState.title.cloneNode(true)
+  transtionFrom(cloneProductTitle, productState.title, product)
+	const cloneProductPrice = productState.price.cloneNode(true)
+  transtionFrom(cloneProductPrice, productState.price, product)
+
+  const animationAll = async () => {
+		if (!webgl[dataModalID]) {
+			const modalWebGL = modal.querySelector('.modal__webgl')
+			const canvas = modalWebGL.querySelector('.canvas')
+			const planeElement = modalWebGL.querySelector('.multi-textures')
+			const navs = modalWebGL.querySelectorAll('[data-goto]')
+			const progress = modalWebGL.querySelector('.progress')
+			webgl[dataModalID] = new WebglSlides({
+				canvas,
+				planeElement,
+				navs,
+				progress
+			})
+		}
+		animate(cloneProductTitle, transitionTo(modalState.title, 'top .8s ease-in .1s, left .8s ease-in .1s, width .8s ease-in .1s, height .8s ease-in .1s, font-size .8s ease-in .1s'), 'width')
+		animate(cloneProductPrice, transitionTo(modalState.price, 'top .9s ease-in .2s, left .9s ease-in .2s, width .9s ease-in .2s, height .9s ease-in .2s, font-size .9s ease-in .2s'), 'width')
+		await animate(cloneProductImg, transitionTo(modalState.img, 'top 1.2s linear .2s, left 1.2s cubic-bezier(0.4, 1, 0.7, 1) .2s, width 1.2s cubic-bezier(0.4, 1, 0.7, 1) .2s, height 1.2s linear .2s'), 'left')
+
+		modal.classList.add('modal--active')
+
+		cloneProductImg.remove()
+		cloneProductTitle.remove()
+		cloneProductPrice.remove()
+		product.style.pointerEvents = ''
+		document.body.classList.remove('is-fade-in')
+	}
+  animationAll()
+}
+
+
+const closeModal = () => {
+	const imagesActive = modalState.modal.querySelectorAll('img')
+	const progress = modalState.modal.querySelector('progress').value
+	const currentActiveImageIndex = `${Math.round((+progress / 100) * (imagesActive.length - 1))}`
+	const src = imagesActive[currentActiveImageIndex].getAttribute('src')
+
+	productState.img.style.backgroundImage = `url(${src})`
+	modalState.img.style.backgroundImage = `url(${src})`
+
+	const cloneProductImg = modalState.img.cloneNode(true)
+	transtionFrom(cloneProductImg, modalState.img, productState.img)
+	const cloneProductTitle = modalState.title.cloneNode(true)	
+	transtionFrom(cloneProductTitle, modalState.title, productState.title)
+	const cloneProductPrice = modalState.price.cloneNode(true)	
+	transtionFrom(cloneProductPrice, modalState.price, productState.price)
+
+	const animationAll = async () => {
+		await asyncImageLoader(src)
+		document.body.classList.add('is-fade-out')
+		animate(cloneProductTitle, transitionTo(productState.title, 'top .8s ease-in .1s, left .8s ease-in .1s, width .8s ease-in .1s, height .8s ease-in .1s, font-size .8s ease-in .1s'), 'width')
+		animate(cloneProductPrice, transitionTo(productState.price, 'top .8s ease-in .15s, left .8s ease-in .15s, width .8s ease-in .15s, height .8s ease-in .15s, font-size .8s ease-in .15s'), 'width')
+		//webgl[currentWebGL].removePlanes()
+		await animate(cloneProductImg, transitionTo(productState.img, 'top 1.5s ease-in 0s, left 1.5s ease-in 0s, width 1.5s ease-in 0s, height 1.5s ease-in 0s'), 'width')
+		productState.product.setAttribute('data-modal-active', false)
+		modalState.modal.classList.remove('modal--active')
+		cloneProductImg.remove()
+		cloneProductTitle.remove()
+		cloneProductPrice.remove()
+		document.body.classList.remove('is-fade-out')
+	}
+
+	animationAll()
 
 }
 
-window.addEventListener("load", () => {
-	const wrapper = document.querySelector('.wrapper')
-	const canvas = wrapper.querySelector('.canvas')
-	const planeElement = wrapper.querySelector('.multi-textures')
-	const navs = wrapper.querySelectorAll('[data-goto]')
-	const progress = wrapper.querySelector('.progress')
-	let slide = new WebglSlides({
-		canvas,
-		planeElement,
-		navs,
-		progress
+const asyncImageLoader = url => {
+	return new Promise( (resolve, reject) => {
+		const image = new Image()
+		image.src = url
+		image.onload = () => resolve(image)
+		image.onerror = () => reject(new Error('could not load image'))
 	})
-
-	// Down, not necesary, only for change Displacements Texture
-	document.querySelector('.js-open-modal').addEventListener('click', () => {
-    document.body.classList.add('modal-active')
-  })
-  document.querySelector('.js-close-modal').addEventListener('click', () => {
-    document.body.classList.remove('modal-active')
-	})
-
-	const settings = document.querySelectorAll('[data-setting]');
-	settings.forEach(setting => {
-		setting.addEventListener('click', event => {
-			const target = event.target;
-			const path = target.getAttribute('src')
-			settings.forEach(setting => setting.classList.remove('active'))
-			target.classList.add('active')
-			document.querySelector('[data-sampler]').src = path
-			progress.value = 16
-
-			slide.removePlanes()
-			slide = new WebglSlides({
-				canvas,
-				planeElement,
-				navs
-			})
-		})
-	})
-});
+}    
